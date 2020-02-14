63f9811fa39715db57890774526d1f2c07ba1f33
宮本貴史 <>
2020-02-14 18:04:01 +0900

    no message

--- node/td.js
+++ node/td.js
@@ -2,6 +2,7 @@ const {execSync} = require('child_process');
 const maxAPI = require('max-api');
 
 const config = { 
+
   "appPath":"/Applications/TouchDesigner099.app",
   "filePath":"/Users/miyamototakashi/Desktop/git_all/2020_musical/test/TD/blurTest.toe"
 }
